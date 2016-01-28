8e7747fe97bdeae6c23377f20ad2b7ad0fb58506
huhudage <lijinhu24048@163.com>
2016-01-28 14:03:46 +0800

    Add several particle system demos.

---
+++ projects/Plugin/ParticleSystem/BlendModes/Scripts/Demo.js
@@ -0,0 +1,34 @@
+// define a user behaviour
+var Test = qc.defineBehaviour('qc.engine.Test', qc.Behaviour, function() {
+    var input = this.game.input,
+        self = this;
+    
+    this.addListener(input.onPointerDown, self.doOnMove, self);
+    this.addListener(input.onPointerMove, self.doOnMove, self);
+    
+    // need this behaviour be scheduled in editor
+    this.runInEditor = true;
+}, {
+    bgImage: qc.Serializer.NODE,
+    particleSystem: qc.Serializer.NODE
+});
+
+// Called when the script instance is being loaded.
+Test.prototype.awake = function() {
+    if (this.bgImage)
+		this.bgImage.phaser.blendMode = Phaser.blendModes.MULTIPLY;
+};
+
+// Called every frame, if the behaviour is enabled.
+Test.prototype.update = function() {
+	if (this.particleSystem) {
+		//this.particleSystem.x = 
+    }
+};
+
+Test.prototype.doOnMove = function(id, x, y) {
+    if (this.particleSystem) {
+        this.particleSystem.x = x;
+    	this.particleSystem.y = y;
+    }
+};
