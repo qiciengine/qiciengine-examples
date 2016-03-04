252048f35b2bae1775826d658efcbbab92655853
linyw <linyw@qcplay.com>
2016-03-04 15:45:02 +0800

    Fix path error for teris demo

--- projects/Project/tetris/Editor/ShapeEditor.js
+++ projects/Project/tetris/Editor/ShapeEditor.js
@@ -1,6 +1,6 @@
 G.extend.menu('Test/Shape', function() {
     var game = G.game;
-    game.assets.load('Assets/prefab/Shape.bin', function(prefab) {
+    game.assets.load('Assets/prefabs/Shape.bin', function(prefab) {
         var shape = game.add.clone(prefab, game.world.find('UIRoot/pool'));
         var c = shape.getScript('qc.tetris.ShapeUI');
 
