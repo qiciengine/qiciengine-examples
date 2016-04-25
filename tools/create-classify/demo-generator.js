15a10798625764135499c17675f9521f95616791
huhudage <lijinhu24048@163.com>
2016-04-25 22:42:05 +0800

    Change path of 'tools' folder.

--- tools/create-classify/demo-generator.js
+++ tools/create-classify/demo-generator.js
@@ -5,8 +5,7 @@
     
 var fs = require('fs'),
     fse = require('fs-extra'),
-    path = require('path'),
-    language;
+    path = require('path');
 
 /**
  * 获取指定目录下的子目录
@@ -37,9 +36,7 @@ function existInArray(arr, ele) {
     return exist;
 }
 
-exports.generate = function(l, destination) {
-    language = l;
-    
+exports.generate = function(projectDir, destination, language) {
     fse.copySync('./menu_' + language, destination);
     
     // 遍历demo收集到的tag存到这个数组里
@@ -51,7 +48,7 @@ exports.generate = function(l, destination) {
     // html页面中的锚点
     var anchors = categories.slice(0);
     // 根目录
-    var root = '../../';
+    var root = projectDir;
     // 读取根目录
     var directories = getDirectories(root);
     for (var i = 0; i < directories.length; i++) {
